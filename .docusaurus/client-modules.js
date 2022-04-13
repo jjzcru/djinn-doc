402e11fba3c693f0eed127d000d0655afd7e39b6
Jose J. Cruz <jjzcru@gmail.com>
2022-04-13 10:32:32 -0400

    First commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/jjzcru/Workspace/Djinn/djinn/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/jjzcru/Workspace/Djinn/djinn/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/jjzcru/Workspace/Djinn/djinn/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/jjzcru/Workspace/Djinn/djinn/src/css/custom.css'),
+];
