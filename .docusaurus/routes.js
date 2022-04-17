e81c547dcd5272f5a243899ccaace3ab37316f1f
Jose J. Cruz <jjzcru@gmail.com>
2022-04-17 19:02:13 -0400

    feat: :sparkles: Add web section for djinn client

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -100,7 +100,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs','cc0'),
+    component: ComponentCreator('/docs','734'),
     routes: [
       {
         path: '/docs/djinn-board/agent',
@@ -151,8 +151,20 @@ export default [
         sidebar: "tutorialSidebar"
       },
       {
+        path: '/docs/djinn-client/gateway',
+        component: ComponentCreator('/docs/djinn-client/gateway','fef'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
         path: '/docs/djinn-client/overview',
         component: ComponentCreator('/docs/djinn-client/overview','c27'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/docs/djinn-client/web',
+        component: ComponentCreator('/docs/djinn-client/web','f7a'),
         exact: true,
         sidebar: "tutorialSidebar"
       },
