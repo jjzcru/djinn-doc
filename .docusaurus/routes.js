aa23ac37298d175f8a3f1682cc84f14af539aa05
Jose J. Cruz <jjzcru@gmail.com>
2022-04-17 19:00:22 -0400

    feat: :sparkles: Add pages for djinn client overview

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -100,7 +100,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs','477'),
+    component: ComponentCreator('/docs','cc0'),
     routes: [
       {
         path: '/docs/djinn-board/agent',
@@ -147,6 +147,12 @@ export default [
       {
         path: '/docs/djinn-board/web-client',
         component: ComponentCreator('/docs/djinn-board/web-client','165'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/docs/djinn-client/overview',
+        component: ComponentCreator('/docs/djinn-client/overview','c27'),
         exact: true,
         sidebar: "tutorialSidebar"
       },
